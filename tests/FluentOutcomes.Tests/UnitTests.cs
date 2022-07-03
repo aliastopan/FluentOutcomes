7f75ac16d0867674c183e935c57b049dd39cbc0b
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-03 19:37:33 +0700

    test: all passes

--- tests/FluentOutcomes.Tests/UnitTests.cs
+++ tests/FluentOutcomes.Tests/UnitTests.cs
@@ -24,4 +24,78 @@ public class UnitTests
 
         Assert.Equal(false, x.IsSuccess);
     }
+
+    [Fact]
+    public void ExpectSucessIfTest()
+    {
+        string? message = "this is a message";
+
+        var x = Outcome
+            .Expect()
+            .SuccessIf(!string.IsNullOrEmpty(message))
+            .Otherwise()
+            .Return();
+
+        Assert.Equal(true, x.IsSuccess);
+    }
+
+    [Fact]
+    public void ExpectFailureIfTest()
+    {
+        string? message = "";
+
+        var x = Outcome
+            .Expect()
+            .FailureIf(string.IsNullOrEmpty(message))
+            .WithError(new Error())
+            .Otherwise()
+            .Return();
+
+        Assert.Equal(false, x.IsSuccess);
+    }
+
+    [Fact]
+    public void GenericExpectSuccessIfTest()
+    {
+        string? message = "this is a message";
+
+        IOutcome<string> x = Outcome
+            .Expect<string>()
+            .SuccessIf(!string.IsNullOrEmpty(message))
+            .Otherwise()
+            .Return(message);
+
+        Assert.Equal(true, x.IsSuccess);
+        Assert.Equal(message, x.Value);
+    }
+
+    [Fact]
+    public void GenericExpectFailureIfTest()
+    {
+        string? message = "";
+
+        IOutcome<string> x = Outcome
+            .Expect<string>()
+            .FailureIf(string.IsNullOrEmpty(message))
+            .WithError(new Error())
+            .Otherwise()
+            .Return(message);
+
+        Assert.Equal(false, x.IsSuccess);
+        Assert.Equal(string.Empty, x.Value);
+    }
+
+    [Fact]
+    public void GenericReturnTypeTest()
+    {
+        string? message = "this is a message";
+
+        IOutcome<string> x = Outcome
+            .Expect<string>()
+            .SuccessIf(!string.IsNullOrEmpty(message))
+            .Otherwise()
+            .Return(message);
+
+        Assert.Equal(typeof(string), message.GetType());
+    }
 }
