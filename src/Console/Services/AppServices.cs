82ebcc4e622550c6647bc97ae618cde69dfe9f53
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-05 23:20:10 +0700

    tests

--- src/Console/Services/AppServices.cs
+++ src/Console/Services/AppServices.cs
@@ -18,5 +18,49 @@ public class AppService : IAppService
     public void Run()
     {
         _logger.LogInformation("Starting...");
+
+        string? foo = "";
+        string? bar = "";
+        string? baz = "";
+        string? qux = "";
+        string? led = "A";
+        string? bam = "";
+        string? dim = "";
+        string? cok = "";
+
+        var actual = foo == "X" && bar == "X" || qux == "X" || led == "" || dim == "X";
+
+        var x = Outcome
+            .Expect()
+            .SuccessIf(foo == "X")
+                .And(bar == "X")
+                .Or(qux == "X")
+                .Or(led == "")
+                .Or(dim == "X")
+            .Otherwise()
+            .Return();
+
+        _logger.LogInformation($"Actual: {actual}");
+        _logger.LogInformation($"Outcome: {x.IsSuccess}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        string surpress = $"{foo} {bar} {baz} {qux} {led} {dim} {bam} {cok}";
+        surpress = "";
     }
 }
