4f7dcb4166d50089d258ff3f1e2cc1b617a1653f
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-20 19:46:59 +0700

    OK

--- src/FluentOutcomes/Settings/OutcomeSettings.cs
+++ src/FluentOutcomes/Settings/OutcomeSettings.cs
@@ -9,7 +9,7 @@ namespace FluentOutcomes.Settings
         private static readonly Lazy<OutcomeSettings> lazy = new Lazy<OutcomeSettings>(() => new OutcomeSettings());
         public static OutcomeSettings Instance => lazy.Value;
 
-        public string AllCorrectMessage { get; internal set; } = "OK.";
+        public string AllCorrectMessage { get; internal set; } = "OK";
         public string DefaultErrorMessage { get; internal set; } = "Unspecified error has occurred.";
         public List<PrefaceMetadata> PrefaceMetadata { get; internal set; }
         public bool UsingStatusResultMetadata { get; internal set; } = false;
